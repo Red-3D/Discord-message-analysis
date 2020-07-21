f36c0cf4dda0c23b0c174c596316398131a71ed4
Red_3D <42870774+Red-3D@users.noreply.github.com>
2020-07-21 14:25:05 +0200

    V2
    put everything into one file and improved the code

---
+++ main.py
@@ -0,0 +1,33 @@
+import json
+import glob
+from matplotlib import pyplot as plt
+
+user_ID = "3062"
+display_name = "Red_3D"
+path = "C:/Users/Red/Documents/Logic World Discord/"
+
+
+files = glob.glob((path+"*.json"))
+print(files)
+
+output = []
+for file in files:
+    data = json.load(open(file, "r", encoding="utf8"))
+    for message in data['messages']:
+        if message['author']['discriminator'] == user_ID:
+            output.append((message['timestamp'])[11:][:2] + '\n')
+
+h = ["00","01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23"]
+h_msg = []
+count = 0
+
+for x in h:
+    for n in output:
+        if(x == n.replace("\n","")):
+            count += 1
+    h_msg.append(count)
+    count = 0
+
+plt.bar(h, h_msg)
+plt.title("Messages from " + display_name + " by hour")
+plt.show()
